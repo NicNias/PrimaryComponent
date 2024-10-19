218e7213a2cb49828d2af1ff25ced4c26214c19a
NaniasNic <ananiasdias0@gmail.com>
2024-10-19 14:59:45 -0300

    cards api

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,15 +2,18 @@ import { Link, useNavigate } from "react-router-dom";
 
 import Button from "../../components/Button";
 
+import { cards } from "../../service/cards";
+
 export default function Home() {
   const navigation = useNavigate();
 
   return (
     <>
-      <h1>Home</h1>
-      <Button title="Cadastro" onClick={() => navigation("/cadastro")} />
-      <Button title="Login" onClick={() => navigation("/login")} /> {/* <--- */}
-      <Link to={"/login"}>Link Login</Link>
+      {cards.map((item) => (
+        <div key={item.id}>
+          <h1>{item.name}</h1>
+        </div>
+      ))}
     </>
   );
 }
---
+++ src/service/cards.js
@@ -0,0 +1,26 @@
+export const cards = [
+  {
+    id: 1,
+    name: "Card 01",
+  },
+  {
+    id: 2,
+    name: "Card 02",
+  },
+  {
+    id: 3,
+    name: "Card 03",
+  },
+  {
+    id: 4,
+    name: "Card 04",
+  },
+  {
+    id: 5,
+    name: "Card 05",
+  },
+  {
+    id: 6,
+    name: "Card 06",
+  },
+];
