ad5308841c298096707dc420db34972265f582d9
NicNias <ananiasdias0@gmail.com>
2024-10-22 17:50:39 -0300

    reafactor: button component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import GlobalStyles from "../src/styles/GlobalStyles";
 import { RouterProvider } from "react-router-dom";
 
 import { paginas } from "./routes/index.routes";
+import Button from "./components/Button";
+
+import { Play } from "@phosphor-icons/react";
 
 export default function App() {
   return (
@@ -10,6 +13,7 @@ export default function App() {
       <GlobalStyles /> {/* Styles Globais usando styled-components */}
       <RouterProvider router={paginas} />{" "}
       {/* Renderizar a lista de Paginas = SPA */}
+      <Button title="Entrar" backGround="secondary" icon={Play} iconSize={20} />
     </>
   );
 }
--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,8 +1,15 @@
 import * as S from "./styles";
 
-export default function Button({ title, backGround, ...rest }) {
+export default function Button({
+  title,
+  backGround,
+  icon: Icon,
+  iconSize,
+  ...rest
+}) {
   return (
     <S.Container {...rest} backGround={backGround}>
+      {Icon && <Icon size={iconSize} />}
       {title}
     </S.Container>
   );
